34b50f6a7c75e5576718749a167969c6f112348c
SIVATHATHAREDDY SATTI <101451780+sivathathareddy-satti@users.noreply.github.com>
2022-03-31 12:32:07 +0530

    Add files via upload

---
+++ 3_Implementation/inc/election.h
@@ -0,0 +1,13 @@
+#ifdef   __ELECTION_H
+#define  __ELECTION_H
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void studentPanel();
+void adminPanel();
+
+#endif//__ELECTION_H
